70adc60f338b81c4dd663c86fcbf06a3bc9d9d1f
bsharp807 <bsharp807@gmail.com>
2019-12-05 14:07:36 +0000

    back to where we were

--- src/containers/CharacterContainer.js
+++ src/containers/CharacterContainer.js
@@ -1,8 +1,8 @@
 import CharacterDisplay from '../characterDisplay/CharacterDisplay';
 import { connect } from 'react-redux';
 
-const RequestHelper = require('./helpers/request_helper');
-const configFile = require('./config/config');
+const RequestHelper = require('../helpers/request_helper');
+const configFile = require('../config/config');
 
 
 const mapStateToProps = (state) => {
@@ -10,29 +10,5 @@ const mapStateToProps = (state) => {
     characters: state.characters
   }
 };
-
-const mapDispatchToProps = (dispatch) => ({
-  addQuestion(data) {
-    dispatch(() => {
-      fetch('http://localhost:3000/questions', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json'}
-      }
-      )
-      .then(res => res.json())
-      .then(questionsData => {
-        dispatch({
-          type: 'LOAD_QUESTIONS_DATA',
-          questionsData
-        })
-        dispatch({
-          type: 'UPDATE_FILTER',
-          filterArray: filteringMechanism(questionsData)
-          })
-      })
-    })
-  }
-})
 
 export default connect(mapStateToProps)(CharacterDisplay);
