818bd93d4bfa776dbed512a21fb015ea7db49a03
bsharp807 <bsharp807@gmail.com>
2019-12-09 10:56:40 +0000

    removed console log

--- src/characterDisplay/CharacterDisplay.js
+++ src/characterDisplay/CharacterDisplay.js
@@ -3,8 +3,6 @@ import CharacterCard from './components/CharacterCard'
 
 class CharacterDisplay extends Component{
   render(){
-    console.log(this.props.character);
-    
     return(
       <div id='character-card-container'>
         <CharacterCard character={this.props.character}/>
