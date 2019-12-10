01656d448e09ccc1d52cf27020b96a5e6789a207
bsharp807 <bsharp807@gmail.com>
2019-12-10 14:54:36 +0000

    post request created

--- src/containers/CreateCharacterContainer.js
+++ src/containers/CreateCharacterContainer.js
@@ -1,11 +1,25 @@
 import CreateCharacterDisplay from '../createCharacterDisplay/CreateCharacterDisplay';
 import { connect } from 'react-redux';
 
+const RequestHelper = require('../helpers/request_helper')
+const configFile = require('../config/config')
+
 const mapStateToProps = (state) => {
   return {
-    characters: state.characters,
     attributes: state.attributes
   }
 };
 
-export default connect(mapStateToProps)(CreateCharacterDisplay);
+const mapDispatchToProps = dispatch => ({
+  postCreatedCharacter(payload){
+    RequestHelper.post(configFile.char, payload)
+    .then((characters) => {
+      dispatch({
+        type: 'UPDATE_SELECTED_CHARACTER',
+        character: characters[characters.length-1]
+      })
+    })
+  }
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(CreateCharacterDisplay);
