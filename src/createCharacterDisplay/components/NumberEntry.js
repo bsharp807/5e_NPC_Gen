1761d8150999e7dcfec4d7a06b2d5de6938de4ad
bsharp807 <bsharp807@gmail.com>
2019-12-10 10:55:07 +0000

    fixed formatting

--- src/createCharacterDisplay/components/NumberEntry.js
+++ src/createCharacterDisplay/components/NumberEntry.js
@@ -3,9 +3,9 @@ import React from 'react'
 const NumberEntry = (props) => {
   return(
     <div id='number-entry-set'>
-    <div id='number-entry-text'>{props.text}</div>
-    <input id='number-entry-input' type="number" value={props.value} onChange={props.handleChange} />
-  </div>
+      <div id='number-entry-text'>{props.text}</div>
+      <input id='number-entry-input' type="number" value={props.value} onChange={props.handleChange} />
+    </div>
   )
 }
 
